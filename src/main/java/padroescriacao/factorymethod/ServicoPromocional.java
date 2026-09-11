2fd3abe6e472075789cd18fc1cb874da5fb97ac0
RaquielRA <ribeiro.raquiel@gmail.com>
2026-09-11 18:18:05 -0300

    Commit para recolocar classes ServicoPromocional que foram excluídas por engano

---
+++ src/main/java/padroescriacao/factorymethod/ServicoPromocional.java
@@ -0,0 +1,12 @@
+package padroescriacao.factorymethod;
+
+public class ServicoPromocional {
+
+    public String ativar() {
+        return "Plano Promocional ativado";
+    }
+
+    public String cancelar() {
+        return "Plano Promocional cancelado";
+    }
+}
